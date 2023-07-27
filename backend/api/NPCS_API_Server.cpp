0e13ddcdc63ff2e290886336fd27d51fc7de0f19
tfergus23 <tomfergus22@gmail.com>
2023-07-27 18:16:26 -0500

    Remove expresscpp; Add tfhttp

--- backend/api/NPCS_API_Server.cpp
+++ backend/api/NPCS_API_Server.cpp
@@ -1,7 +1,8 @@
 #include "api/NPCS_API_Server.hpp"
 #include "nlohmann/json.hpp"
 #include "sim/data/Species.hpp"
-
+#include <chrono>
+#include <thread>
 
 using json = nlohmann::json;
 
@@ -10,95 +11,47 @@ using json = nlohmann::json;
 #define ALLOWED_HEADERS "Authorization,Content-Type,X-Requested-With"
 #define ALLOWED_METHODS "GET,POST,PUT,DELETE,OPTIONS"
 
-NPCS_API_Server::NPCS_API_Server() : m_Expresscpp{std::make_shared<expresscpp::ExpressCpp>()}, m_SpeciesData{getSpeciesData()}{
-    expresscpp::Console::setLogLevel(expresscpp::LogLevel::kDebug);
-    auto headerMiddleWare = [&](auto req, auto res, auto next) {
-        res->res.set("Access-Control-Allow-Origin", "*");
-        next();
-    };
-    auto authorizedMiddleWare = [&](auto req, auto res, auto next) {
-        std::string authorization = req->getHeader("Authorization");
-        std::string tokenName = usernameFromToken(authorization);
-        std::string requestName = req->GetParams().at("username");
-        if (tokenName == requestName){
-            next();
-        }
-        else{
-            json response;
-            response["success"] = false;
-            response["message"] = "Unauthorized";
-            res->SetStatus(401);
-            res->Send(response.dump());
-        }
-    };
-    m_Expresscpp->Use(headerMiddleWare);
-    auto router = m_Expresscpp->GetRouter("noauth_api");
-    auto authRouter = m_Expresscpp->GetRouter("auth_api");
-    m_Expresscpp->Use("/api", router);
-    router->Use("/user/:username", authRouter);
-    authRouter->Use(authorizedMiddleWare);
+bool addCORSHeaderMiddleware(const HTTP_Request& req, HTTP_Response& res){
+    res.headers["Access-Control-Allow-Origin"] = "*";
+    return true;
+}
 
-    addPreflightHandler(router, "/auth");
-    router->Post("/auth", [=](auto req, auto res) {
-        json response;
-        try{
-            json body = json::parse(req->getBody());
-            std::string username = body["username"].get<std::string>();
-            std::string password = body["password"].get<std::string>();
-            std::string token = authenticateUser(username, password);
-            if (token != ""){
-                response["success"] = true;
-                response["token"] = token;
-            }
-            else{
-                response["success"] = false;
-                response["message"] = "Invalid credentials";
-                res->SetStatus(401);
-            }
-        }
-        catch (std::exception& e){
-            response["success"] = false;
-            response["message"] = "Bad request";
-            res->SetStatus(400);
-        }
-        res->Json(response.dump());
+void preflightHandler(const HTTP_Request& req, HTTP_Response& res){
+    res.headers["Access-Control-Allow-Methods"] = ALLOWED_METHODS;
+    res.headers["Access-Control-Allow-Headers"] = ALLOWED_HEADERS;
+    res.Send("");
+}
+
+bool authMiddleware(const HTTP_Request& req, HTTP_Response& res){
+    return true;
+}
+
+NPCS_API_Server::NPCS_API_Server() : m_SpeciesData{getSpeciesData()}{
+    //Create routes
+    Route* baseRoute = app.Create_Route("/api");
+    Route* authorizedRoute = app.Create_Route("/api/user/:username");
+
+    //Add middlewares
+    app.base_route.Use(addCORSHeaderMiddleware);
+    baseRoute->Use(addCORSHeaderMiddleware);
+    authorizedRoute->Use(addCORSHeaderMiddleware);
+    authorizedRoute->Use(authMiddleware);
+
+    //Add preflight handler to all paths
+    app.Add_Handler("OPTIONS", "*", preflightHandler);
+
+    //Add API handlers 
+    app.Add_Handler("GET", authorizedRoute, "/", [](const HTTP_Request& req, HTTP_Response& res) {
+        res.Send("Authorized route!");
     });
 
-    addPreflightHandler(router, "/user/:username");
-    authRouter->Get("/", [](auto req, auto res) {
-        json response;
-        std::string username = req->GetParams().at("username");
-        if (username == "BilboSwaggins"){
-            response["name"] = username;
-            response["id"] = 1;
-            response["success"] = true;
-            response["token"] = "BilboSwaggins:abc";
-        }
-        else{
-            response["success"] = false;
-        }
-        res->Json(response.dump());
-    });
-    addPreflightHandler(router, "/user/:username/logout");
-    authRouter->Put("/logout", [](auto req, auto res) {
-        res->Send();
-        return;
-        json response;
-        //TODO: Set token to null in database
-        response["success"] = true;
-        res->Json(response.dump());
-    });
-    /*
-    router->Get("/sim/tournament/:id", [](auto req, auto res) {res->Send("WIP!");});
-    router->Get("/sim/battle/:id", [](auto req, auto res) {res->Send("WIP!");});
-    router->Post("/sim/tournament", [](auto req, auto res) {res->Send("WIP!");});
-    router->Post("/sim/battle", [](auto req, auto res) {res->Send("WIP!");});
-    */
-   m_Expresscpp->Stack();
+    app.Add_Handler("GET", baseRoute, "/", [](const HTTP_Request& req, HTTP_Response& res) {
+        res.Send("Base route!");
+    }); 
 }
 
 int NPCS_API_Server::run(){
-    m_Expresscpp->Listen(PORT,[=](auto /*ec*/) { std::cout << "Listening on port " << PORT << "\n\n"; }).Run();
+    app.Listen(8080);
     return 0;
 }
 
@@ -106,15 +59,7 @@ std::string NPCS_API_Server::authenticateUser(const std::string& username, const
     if (username == "BilboSwaggins" && password == "yourmom") return "BilboSwaggins:abc";
     return "";
 }
-//Expresscpp doesn't support wildcards apparently so we need to manually add a handler for every preflight request...
-void NPCS_API_Server::addPreflightHandler(expresscpp::RouterPtr router, const std::string& path){
-    router->RegisterPath(path, expresscpp::HttpMethod::Options, [=](auto req, auto res){
-        res->res.set("Access-Control-Allow-Methods", ALLOWED_METHODS);
-        res->res.set("Access-Control-Allow-Headers", ALLOWED_HEADERS);
-        res->SetStatus(200);
-        res->Send();
-    });
-}
+
 
 bool NPCS_API_Server::isTokenValid(const std::string token){
     return token == "BilboSwaggins:abc";
--- backend/api/NPCS_API_Server.hpp
+++ backend/api/NPCS_API_Server.hpp
@@ -1,19 +1,20 @@
 #pragma once
 #include "sim/battle/Battle.hpp"
-#include "expresscpp/expresscpp.hpp"
+#include "tfhttp/HTTP_Server.hpp"
+using namespace tfhttp;
 
 class NPCS_API_Server{
 public:
     NPCS_API_Server();
     int run();
 private:
-    const std::shared_ptr<expresscpp::ExpressCpp> m_Expresscpp;
+    HTTP_Server app;
     std::string authenticateUser(const std::string& username, const std::string& password);
     std::string getSpeciesData();
     std::string getAbilityData();
     std::string getItemData();
     std::string getMoveData();
-    void addPreflightHandler(expresscpp::RouterPtr router, const std::string& path);
+    //void addPreflightHandler(expresscpp::RouterPtr router, const std::string& path);
     bool isTokenValid(const std::string token);
     const std::string m_SpeciesData;
     const std::string m_AbilityData;
