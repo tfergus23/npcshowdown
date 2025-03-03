6d04b855b7fecafa1e78e9fcdbd268e6a0ecf4d4
tfergus23 <tomfergus22@gmail.com>
2025-03-02 22:25:05 -0600

    Change NONE type to be 0; add static assertions for the type chart

--- backend/sim/battle/Type.cpp
+++ backend/sim/battle/Type.cpp
@@ -1,30 +1,13 @@
 #include "sim/battle/Type.hpp"
 
-static const float typeChart[19][19] = {
-    {1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,0.5f,0.0f,1.0f,1.0f,0.5f,1.0f,1.0f},
-    {1.0f,0.5f,0.5f,1.0f,2.0f,2.0f,1.0f,1.0f,1.0f,1.0f,1.0f,2.0f,0.5f,1.0f,0.5f,1.0f,2.0f,1.0f,1.0f},
-    {1.0f,2.0f,0.5f,1.0f,0.5f,1.0f,1.0f,1.0f,2.0f,1.0f,1.0f,1.0f,2.0f,1.0f,0.5f,1.0f,1.0f,1.0f,1.0f},
-    {1.0f,1.0f,2.0f,0.5f,0.5f,1.0f,1.0f,1.0f,0.0f,2.0f,1.0f,1.0f,1.0f,1.0f,0.5f,1.0f,1.0f,1.0f,1.0f},
-    {1.0f,0.5f,2.0f,1.0f,0.5f,1.0f,1.0f,0.5f,2.0f,0.5f,1.0f,0.5f,2.0f,1.0f,0.5f,1.0f,0.5f,1.0f,1.0f},
-    {1.0f,0.5f,0.5f,1.0f,2.0f,0.5f,1.0f,1.0f,2.0f,2.0f,1.0f,1.0f,1.0f,1.0f,2.0f,1.0f,0.5f,1.0f,1.0f},
-    {2.0f,1.0f,1.0f,1.0f,1.0f,2.0f,1.0f,0.5f,1.0f,0.5f,0.5f,0.5f,2.0f,0.0f,1.0f,2.0f,2.0f,1.0f,0.5f},
-    {1.0f,1.0f,1.0f,1.0f,2.0f,1.0f,1.0f,0.5f,0.5f,1.0f,1.0f,1.0f,0.5f,0.5f,1.0f,1.0f,0.0f,1.0f,2.0f},
-    {1.0f,2.0f,1.0f,2.0f,0.5f,1.0f,1.0f,2.0f,1.0f,0.0f,1.0f,0.5f,2.0f,1.0f,1.0f,1.0f,2.0f,1.0f,1.0f},
-    {1.0f,1.0f,1.0f,0.5f,2.0f,1.0f,2.0f,1.0f,1.0f,1.0f,1.0f,2.0f,0.5f,1.0f,1.0f,1.0f,0.5f,1.0f,1.0f},
-    {1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,2.0f,2.0f,1.0f,1.0f,0.5f,1.0f,1.0f,1.0f,1.0f,0.0f,0.5f,1.0f,1.0f},
-    {1.0f,0.5f,1.0f,1.0f,2.0f,1.0f,0.5f,0.5f,1.0f,0.5f,2.0f,1.0f,1.0f,0.5f,1.0f,2.0f,0.5f,1.0f,0.5f},
-    {1.0f,2.0f,1.0f,1.0f,1.0f,2.0f,0.5f,1.0f,0.5f,2.0f,1.0f,2.0f,1.0f,1.0f,1.0f,1.0f,0.5f,1.0f,1.0f},
-    {0.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,2.0f,1.0f,1.0f,2.0f,1.0f,0.5f,0.5f,1.0f,1.0f},
-    {1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,2.0f,1.0f,0.5f,1.0f,0.0f},
-    {1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,0.5f,1.0f,1.0f,1.0f,2.0f,1.0f,1.0f,2.0f,1.0f,0.5f,0.5f,1.0f,0.5f},
-    {1.0f,0.5f,0.5f,0.5f,1.0f,2.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,2.0f,1.0f,1.0f,1.0f,0.5f,1.0f,2.0f},
-    {1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f},
-    {1.0f,0.5f,1.0f,1.0f,1.0f,1.0f,2.0f,0.5f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,2.0f,2.0f,0.5f,1.0f,1.0f}
-};
-
-float typeMatchup(Type attackingType, Type defendingType1, Type defendingType2){
-    int attackingIndex = (int) attackingType;
-    int defendingIndex1 = (int) defendingType1;
-    int defendingIndex2 = (int) defendingType2;
-    return typeChart[attackingIndex][defendingIndex1] * typeChart[attackingIndex][defendingIndex2];
-}
+static_assert(typeMatchup(Type::FIRE, Type::WATER, Type::NONE) == NOT_VERY_EFFECTIVE);
+static_assert(typeMatchup(Type::FIRE, Type::GRASS, Type::NONE) == SUPER_EFFECTIVE);
+static_assert(typeMatchup(Type::GRASS, Type::WATER, Type::GROUND) == ULTRA_EFFECTIVE);
+static_assert(typeMatchup(Type::NORMAL, Type::GHOST, Type::STEEL) == NOT_EFFECTIVE);
+static_assert(typeMatchup(Type::ELECTRIC, Type::GHOST, Type::STEEL) == NORMAL_EFFECTIVE);
+static_assert(typeMatchup(Type::NONE, Type::GHOST, Type::NONE) == NORMAL_EFFECTIVE);
+static_assert(typeMatchup(Type::NONE, Type::NONE, Type::NONE) == NORMAL_EFFECTIVE);
+static_assert(typeMatchup(Type::FIGHTING, Type::NORMAL, Type::GHOST) == NOT_EFFECTIVE);
+static_assert(typeMatchup(Type::FIGHTING, Type::NORMAL, Type::NONE) == SUPER_EFFECTIVE);
+static_assert(typeMatchup(Type::ELECTRIC, Type::WATER, Type::FLYING) == ULTRA_EFFECTIVE);
+static_assert(typeMatchup(Type::NORMAL, Type::ROCK, Type::STEEL) == BARELY_EFFECTIVE);
--- backend/sim/battle/Type.hpp
+++ backend/sim/battle/Type.hpp
@@ -1,9 +1,36 @@
 #pragma once
 #include <stdint.h>
 
-enum class Type : uint8_t {NORMAL,FIRE,WATER,ELECTRIC,GRASS,ICE,FIGHTING,POISON,GROUND,FLYING,PSYCHIC,BUG,ROCK,GHOST,DRAGON,DARK,STEEL,NONE,FAIRY};
+enum class Type : uint8_t {NONE,NORMAL,FIRE,WATER,ELECTRIC,GRASS,ICE,FIGHTING,POISON,GROUND,FLYING,PSYCHIC,BUG,ROCK,GHOST,DRAGON,DARK,STEEL,FAIRY};
 
-float typeMatchup(Type attackingType, Type defendingType1, Type defendingType2);
+inline constexpr float typeChart[19][19] = {
+    {1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f},
+    {1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,0.5f,0.0f,1.0f,1.0f,0.5f,1.0f},
+    {1.0f,1.0f,0.5f,0.5f,1.0f,2.0f,2.0f,1.0f,1.0f,1.0f,1.0f,1.0f,2.0f,0.5f,1.0f,0.5f,1.0f,2.0f,1.0f},
+    {1.0f,1.0f,2.0f,0.5f,1.0f,0.5f,1.0f,1.0f,1.0f,2.0f,1.0f,1.0f,1.0f,2.0f,1.0f,0.5f,1.0f,1.0f,1.0f},
+    {1.0f,1.0f,1.0f,2.0f,0.5f,0.5f,1.0f,1.0f,1.0f,0.0f,2.0f,1.0f,1.0f,1.0f,1.0f,0.5f,1.0f,1.0f,1.0f},
+    {1.0f,1.0f,0.5f,2.0f,1.0f,0.5f,1.0f,1.0f,0.5f,2.0f,0.5f,1.0f,0.5f,2.0f,1.0f,0.5f,1.0f,0.5f,1.0f},
+    {1.0f,1.0f,0.5f,0.5f,1.0f,2.0f,0.5f,1.0f,1.0f,2.0f,2.0f,1.0f,1.0f,1.0f,1.0f,2.0f,1.0f,0.5f,1.0f},
+    {1.0f,2.0f,1.0f,1.0f,1.0f,1.0f,2.0f,1.0f,0.5f,1.0f,0.5f,0.5f,0.5f,2.0f,0.0f,1.0f,2.0f,2.0f,0.5f},
+    {1.0f,1.0f,1.0f,1.0f,1.0f,2.0f,1.0f,1.0f,0.5f,0.5f,1.0f,1.0f,1.0f,0.5f,0.5f,1.0f,1.0f,0.0f,2.0f},
+    {1.0f,1.0f,2.0f,1.0f,2.0f,0.5f,1.0f,1.0f,2.0f,1.0f,0.0f,1.0f,0.5f,2.0f,1.0f,1.0f,1.0f,2.0f,1.0f},
+    {1.0f,1.0f,1.0f,1.0f,0.5f,2.0f,1.0f,2.0f,1.0f,1.0f,1.0f,1.0f,2.0f,0.5f,1.0f,1.0f,1.0f,0.5f,1.0f},
+    {1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,2.0f,2.0f,1.0f,1.0f,0.5f,1.0f,1.0f,1.0f,1.0f,0.0f,0.5f,1.0f},
+    {1.0f,1.0f,0.5f,1.0f,1.0f,2.0f,1.0f,0.5f,0.5f,1.0f,0.5f,2.0f,1.0f,1.0f,0.5f,1.0f,2.0f,0.5f,0.5f},
+    {1.0f,1.0f,2.0f,1.0f,1.0f,1.0f,2.0f,0.5f,1.0f,0.5f,2.0f,1.0f,2.0f,1.0f,1.0f,1.0f,1.0f,0.5f,1.0f},
+    {1.0f,0.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,2.0f,1.0f,1.0f,2.0f,1.0f,0.5f,0.5f,1.0f},
+    {1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,2.0f,1.0f,0.5f,0.0f},
+    {1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,0.5f,1.0f,1.0f,1.0f,2.0f,1.0f,1.0f,2.0f,1.0f,0.5f,0.5f,0.5f},
+    {1.0f,1.0f,0.5f,0.5f,0.5f,1.0f,2.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,2.0f,1.0f,1.0f,1.0f,0.5f,2.0f},
+    {1.0f,1.0f,0.5f,1.0f,1.0f,1.0f,1.0f,2.0f,0.5f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,2.0f,2.0f,0.5f,1.0f}
+};
+
+constexpr float typeMatchup(Type attackingType, Type defendingType1, Type defendingType2){
+    int attackingIndex = (int) attackingType;
+    int defendingIndex1 = (int) defendingType1;
+    int defendingIndex2 = (int) defendingType2;
+    return typeChart[attackingIndex][defendingIndex1] * typeChart[attackingIndex][defendingIndex2];
+}
 
 constexpr float ULTRA_EFFECTIVE = 4.0f;
 constexpr float SUPER_EFFECTIVE = 2.0f;
