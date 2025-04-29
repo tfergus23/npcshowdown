e04f78aa052dcdebcb528b3f4f43db37469eacfa
tfergus23 <tomfergus22@gmail.com>
2025-04-29 15:17:57 -0500

    Statically link mariadbcpp

--- backend/api/MariaDBConnection.hpp
+++ backend/api/MariaDBConnection.hpp
@@ -2,7 +2,7 @@
 #include "sim/battle/Battle.hpp"
 #include "sim/tournament/BattleResult.hpp"
 #include "sim/tournament/Tournament.hpp"
-#include "mariadb/conncpp.hpp"
+#include "conncpp.hpp"
 #include "api_utils.hpp"
 #include <optional>
 
