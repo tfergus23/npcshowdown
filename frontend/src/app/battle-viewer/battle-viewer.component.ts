92206489af32d37fb9c5909630f3cd570cbf3566
tfergus23 <tomfergus22@gmail.com>
2025-05-08 11:35:36 -0500

    Add tween and animation system

--- frontend/src/app/battle-viewer/battle-viewer.component.ts
+++ frontend/src/app/battle-viewer/battle-viewer.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { Application, Assets, Point, Sprite, Ticker } from 'pixi.js';
+import { Application, Assets, Point, Sprite, Ticker, Text, ContainerChild } from 'pixi.js';
 
 const HORIZONTAL_PADDING_FACTOR = 4.5;
 const POKEMON_SCALE = 2.5;
@@ -35,6 +35,13 @@ export class BattleViewerComponent {
   trainer2Textures: Array<any> = [];
   poke1: Sprite = new Sprite();
   poke2: Sprite = new Sprite();
+  weatherLabel: Text = new Text({
+    text: 'Weather:',
+    style: {
+      fontFamily: 'Unageo-Bold',
+      fontSize: 35
+    }
+  });
   app: Application = new Application();
 
   ngOnInit(){
@@ -68,7 +75,13 @@ export class BattleViewerComponent {
           this.app.stage.addChild(this.poke1);
           this.app.stage.addChild(this.poke2);
 
-          this.poke1.y = this.app.screen.height / 2;
+          await Assets.load('/assets/Unageo-Bold.ttf');
+
+          this.weatherLabel.anchor.set(0.5);
+          this.app.stage.addChild(this.weatherLabel);
+
+          this.startAnimation(this.poke2MeleeAttack);
+          
       
           // Listen for animate update
           this.app.ticker.add((time) =>
@@ -85,11 +98,132 @@ export class BattleViewerComponent {
     return poke.toLowerCase() + ".png";
   }
 
-  poke1ScreenPos: Point = new Point(0.25, 0.5);
-  poke2ScreenPos: Point = new Point(0.75, 0.5);
+  weatherLabelScreenPos: Point = new Point(0.5, 0.1);
+  tweens: Array<Tween> = new Array<Tween>();
+  animations: Array<Animation> = new Array<Animation>();
+
+  poke1Home: Point = new Point(0.25, 0.5);
+  poke2Home: Point = new Point(0.75, 0.5);
+
+  poke1ScreenPos: Point = new Point(this.poke1Home.x, this.poke1Home.y);
+  poke2ScreenPos: Point = new Point(this.poke2Home.x, this.poke2Home.y);
+
+  poke1MeleeAttack = new Animation([
+    new Tween(this.poke1ScreenPos, new Point(this.poke2ScreenPos.x - 0.10, this.poke2ScreenPos.y), 700),
+    new Tween(this.poke1ScreenPos, new Point(this.poke2ScreenPos.x - 0.15, this.poke2ScreenPos.y), 500),
+    new Tween(this.poke1ScreenPos, new Point(this.poke2ScreenPos.x, this.poke2ScreenPos.y), 150),
+    new Tween(this.poke1ScreenPos, new Point(this.poke2ScreenPos.x - 0.15, this.poke2ScreenPos.y), 150),
+    new Tween(this.poke1ScreenPos, new Point(this.poke2ScreenPos.x - 0.15, this.poke2ScreenPos.y), 50),
+    new Tween(this.poke1ScreenPos, this.poke1Home, 700),
+  ]);
+
+  poke2MeleeAttack = new Animation([
+    new Tween(this.poke2ScreenPos, new Point(this.poke1ScreenPos.x + 0.10, this.poke1ScreenPos.y), 700),
+    new Tween(this.poke2ScreenPos, new Point(this.poke1ScreenPos.x + 0.15, this.poke1ScreenPos.y), 500),
+    new Tween(this.poke2ScreenPos, new Point(this.poke1ScreenPos.x, this.poke1ScreenPos.y), 150),
+    new Tween(this.poke2ScreenPos, new Point(this.poke1ScreenPos.x + 0.15, this.poke1ScreenPos.y), 150),
+    new Tween(this.poke2ScreenPos, new Point(this.poke1ScreenPos.x + 0.15, this.poke1ScreenPos.y), 50),
+    new Tween(this.poke2ScreenPos, this.poke2Home, 700),
+  ]);
+
+  screenToWorldPos(point: Point) : Point{
+    return new Point(this.app.screen.width * point.x, this.app.screen.height * point.y);
+  }
 
   update(time: Ticker){
-    this.poke1.position = new Point(this.app.screen.width * this.poke1ScreenPos.x, this.app.screen.height * this.poke1ScreenPos.y);
-    this.poke2.position = new Point(this.app.screen.width * this.poke2ScreenPos.x, this.app.screen.height * this.poke2ScreenPos.y);
+    this.updateTweens(time.deltaMS);
+    this.updateAnimations();
+
+    this.updateWorldPositions();
+  }
+
+  updateTweens(dt: number){
+    this.tweens.forEach((tween, i) =>{
+      tween.update(dt);
+    });
+    this.tweens = this.tweens.filter(tween => !tween.done);
+  }
+
+  updateWorldPositions(){
+    this.poke1.position = this.screenToWorldPos(this.poke1ScreenPos);
+    this.poke2.position = this.screenToWorldPos(this.poke2ScreenPos);
+    this.weatherLabel.position = this.screenToWorldPos(this.weatherLabelScreenPos);
+  }
+
+  updateAnimations(){
+    this.animations.forEach(anim => {
+      anim.update(this.tweens);
+    });
+
+    this.animations = this.animations.filter(anim => !anim.done);
+  }
+
+  startAnimation(anim: Animation){
+    this.animations.push(anim);
+    anim.start(this.tweens);
+  }
+}
+
+class Tween{
+  object: Point;
+  start: Point;
+  dest: Point;
+  elapsed: number = 0.0;
+  totalTime: number;
+  done: boolean = false;
+  started: boolean = false;
+  constructor(object: Point, dest: Point, time: number){
+    this.object = object;
+    this.start = new Point(object.x ,object.y);
+    this.dest = dest;
+    this.totalTime = time;
+  }
+
+  update(dt: number){
+    if (!this.started){
+      this.start = new Point(this.object.x, this.object.y);
+      this.started = true;
+    }
+    if (this.done) return;
+
+    this.elapsed += dt;
+    
+    if (this.elapsed >= this.totalTime){
+      this.object.x = this.dest.x;
+      this.object.y = this.dest.y;
+      this.done = true;
+      return;
+    }
+    let fullLength: Point = new Point(this.dest.x - this.start.x, this.dest.y - this.start.y);
+    let percentTraveled = this.elapsed / this.totalTime;
+    let distanceTraveled: Point = new Point(fullLength.x * percentTraveled, fullLength.y * percentTraveled);
+    this.object.x = this.start.x + distanceTraveled.x;
+    this.object.y = this.start.y + distanceTraveled.y;
+  }
+}
+
+class Animation{
+  tweens: Array<Tween>;
+  currentTween: number  = 0;
+  done: boolean = false;
+  constructor(tweens: Array<Tween>){
+    this.tweens = tweens;
+  }
+
+  start(stageTweens:Array<Tween>){
+    stageTweens.push(this.tweens[this.currentTween]);
+  }
+
+  update(stageTweens:Array<Tween>){
+    if (this.done) return;
+
+    if (this.tweens[this.currentTween].done){
+      this.currentTween++;
+      if (this.currentTween >= this.tweens.length){
+        this.done = true;
+        return;
+      }
+      stageTweens.push(this.tweens[this.currentTween]);
+    }
   }
 }
