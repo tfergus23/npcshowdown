29b298f1b7662f4fcd514e405c17350f83c50d6b
tfergus23 <tomfergus22@gmail.com>
2025-05-09 17:18:11 -0500

    Add framework for test battles

---
+++ backend/tests/sim/TestBattles.hpp
@@ -0,0 +1,32 @@
+#include "gtest/gtest.h"
+#include <filesystem>
+#include <fstream>
+#include "nlohmann/json.hpp"
+#include "api/api_utils.hpp"
+
+
+using json = nlohmann::json;
+
+TEST(TestBattles, TestBattles){
+    namespace fs = std::filesystem;
+    std::string folderPath = "./test_battles";
+
+    for (const auto& entry : fs::directory_iterator(folderPath)) {
+        if (fs::is_regular_file(entry.status())) {
+            const fs::path& filePath = entry.path();
+            std::ifstream file(filePath, std::ios::binary);
+
+            ASSERT_TRUE(file);
+            std::string contents((std::istreambuf_iterator<char>(file)),std::istreambuf_iterator<char>());
+            json battleJson = json::parse(contents);
+            ASSERT_EQ(validateBattleRequest(battleJson), "");
+
+            Battle battle(battleJson["trainer1"], battleJson["trainer2"], seedFromString(battleJson["seed"]));
+            battle.simulate();
+            EXPECT_FALSE(battle.invalid);
+            if (battle.invalid){
+                ADD_FAILURE() << "Test battle failed: " + filePath.string();
+            }
+        }
+    }
+}
--- backend/tests/tests.cpp
+++ backend/tests/tests.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "tests/sim/DamageCalc.hpp"
+#include "tests/sim/TestBattles.hpp"
 
 int main(int argc, char** argv){
     testing::InitGoogleTest(&argc, argv);
