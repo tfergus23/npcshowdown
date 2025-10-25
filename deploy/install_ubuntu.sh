834de22e8730bd514757484274082bcf60e43560
tfergus23 <tomfergus22@gmail.com>
2025-10-25 17:24:00 -0500

    Install script now installs nginx

--- deploy/install_ubuntu.sh
+++ deploy/install_ubuntu.sh
@@ -2,6 +2,19 @@ if [ "$EUID" -ne 0 ]; then
   echo "ERROR: Please run the install script as root."
   exit 1
 fi
+while true; do
+  read -sp "Enter database account password: " dbpassword
+  echo
+  read -sp "Enter database account password (Repeat): " dbpassword2
+  echo
+
+  if [[ "$dbpassword" == "$dbpassword2" ]]; then
+    unset dbpassword2
+    break
+  fi
+  echo "Passwords do not match."
+  unset dbpassword dbpassword2
+done
 
 mkdir -p /npcs
 mv npcs-ubuntu-x64-*.tar.gz /npcs
@@ -11,15 +24,15 @@ rm npcs-ubuntu-x64-*.tar.gz
 touch npcs_config.ini
 
 echo "tournament_threads=8" >> npcs_config.ini
-echo "db_user=root" >> npcs_config.ini
-echo "db_password=" >> npcs_config.ini
+echo "db_user=svcnpcshowdown" >> npcs_config.ini
+echo "db_password=password" >> npcs_config.ini
 echo "db_host=localhost" >> npcs_config.ini
-echo "db_name=npcs_test" >> npcs_config.ini
-echo "db_port=3307" >> npcs_config.ini
+echo "db_name=npcs_prod" >> npcs_config.ini
+echo "db_port=3306" >> npcs_config.ini
 echo "max_user_sessions=10" >> npcs_config.ini
 echo "max_trainers_per_user=150" >> npcs_config.ini
-echo "website_url=http://localhost:4200" >> npcs_config.ini
-echo "api_port=3000" >> npcs_config.ini
+echo "website_url=http://192.168.56.101" >> npcs_config.ini
+echo "api_port=8080" >> npcs_config.ini
 echo "serve_static=0" >> npcs_config.ini
 echo "static_dir=./static/" >> npcs_config.ini
 echo "keep_tournament_days=7" >> npcs_config.ini
@@ -29,7 +42,7 @@ echo "max_signups_per_day=3" >> npcs_config.ini
 sudo useradd --system --no-create-home --shell /usr/sbin/nologin svcnpcshowdown
 
 sudo chown -R svcnpcshowdown:svcnpcshowdown /npcs
-sudo chmod -R 500 /npcs
+sudo chmod -R 755 /npcs
 sudo chmod 500 npcshowdown
 sudo chmod 600 npcs_config.ini
 
@@ -38,12 +51,10 @@ cd -
 sudo cp npcshowdown.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable npcshowdown.service
-sudo setcap 'cap_net_bind_service=+ep' /npcs/npcshowdown
-
-exit 0
 
 sudo apt update
 sudo apt upgrade -y
+sudo unattended-upgrades
 
 sudo apt install vim -y
 
@@ -60,4 +71,20 @@ sudo apt install mariadb-server -y
 sudo systemctl enable mariadb
 sudo mysql_secure_installation
 
+echo >> create-tables-prod.sql
+echo "CREATE USER 'svcnpcshowdown'@'localhost' IDENTIFIED BY '$dbpassword';" >> create-tables-prod.sql
+echo "GRANT ALL PRIVILEGES ON npcs_prod.* TO 'svcnpcshowdown'@'localhost';" >> create-tables-prod.sql
+echo "FLUSH PRIVILEGES;" >> create-tables-prod.sql
+
 sudo mariadb -u root < create-tables-prod.sql
+
+sudo apt install nginx -y
+sudo cp npcshowdown.conf /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/npcshowdown.conf /etc/nginx/sites-enabled/
+sudo rm /etc/nginx/sites-enabled/default
+sudo nginx -t
+if [ $? -ne 0 ]; then
+  echo "Problem with nginx configuration. Stopping."
+  exit 1
+fi
+sudo chown -R www-data:www-data /npcs/static
